2a1295ffce99a3cefd56f1387f3ff85fc36d0b24
Makentoshe <mkliverout@gmail.com>
2020-02-19 21:17:32 +0300

    [feature] Add encode/decode buttons behavior

--- app/src/main/java/com/makentoshe/chiperchan/view/cipher/CipherFragment.kt
+++ app/src/main/java/com/makentoshe/chiperchan/view/cipher/CipherFragment.kt
@@ -4,22 +4,58 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.makentoshe.chiperchan.R
 import com.makentoshe.chiperchan.model.Cipher
 import com.makentoshe.chiperchan.ui.cipher.CipherFragmentUi
 import toothpick.ktp.delegate.inject
+import java.io.Serializable
 
 class CipherFragment : Fragment() {
 
     val arguments = Arguments(this)
     private val cipherFactory by inject<Cipher.Factory>()
+    private var action: Action = Action.Encode
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) return
+        action = savedInstanceState.get(Action::class.java.simpleName) as Action
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return CipherFragmentUi(cipherFactory).create(requireContext())
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        println(cipherFactory.title)
+        val encodeButton = view.findViewById<Button>(R.id.cipher_fragment_encode)
+        val decodeButton = view.findViewById<Button>(R.id.cipher_fragment_decode)
+        val inputEditText = view.findViewById<TextInputEditText>(R.id.cipher_fragment_input_edittext)
+        val inputLayout = view.findViewById<TextInputLayout>(R.id.cipher_fragment_input)
+
+        encodeButton.setOnClickListener {
+            encodeButton.isEnabled = false
+            decodeButton.isEnabled = true
+            action = Action.Encode
+            inputLayout.hint = getString(R.string.encode)
+        }
+        decodeButton.setOnClickListener {
+            decodeButton.isEnabled = false
+            encodeButton.isEnabled = true
+            action = Action.Decode
+            inputLayout.hint = getString(R.string.decode)
+        }
+        when (action) {
+            is Action.Encode -> encodeButton.performClick()
+            is Action.Decode -> decodeButton.performClick()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putSerializable(Action::class.java.simpleName, action)
     }
 
     class Factory {
@@ -30,20 +66,29 @@ class CipherFragment : Fragment() {
         }
     }
 
-    class Arguments(fragment: CipherFragment) {
+    class Arguments(private val cipherFragment: CipherFragment) {
 
         init {
-            (fragment as Fragment).arguments = Bundle()
+            val fragment = cipherFragment as Fragment
+            if (fragment.arguments == null) {
+                fragment.arguments = Bundle()
+            }
         }
 
-        private val fragmentArguments = fragment.requireArguments()
+        private val fragmentArguments: Bundle
+            get() = cipherFragment.requireArguments()
 
-        var cipherTitle: String?
+        var cipherTitle: String
             set(value) = fragmentArguments.putString(TITLE, value)
-            get() = fragmentArguments.getString(TITLE)
+            get() = fragmentArguments.getString(TITLE)!!
 
         companion object {
             private const val TITLE = "Title"
         }
     }
+}
+
+sealed class Action : Serializable {
+    object Encode : Action(), Serializable
+    object Decode : Action(), Serializable
 }
