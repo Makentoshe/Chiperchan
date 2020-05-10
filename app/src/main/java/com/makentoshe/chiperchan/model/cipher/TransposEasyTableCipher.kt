267c160fbf7415c54f4a02e3e99fd0a2d580461e
RGBee-4x <RGBee-4x@users.noreply.github.com>
2020-05-10 22:30:30 +0300

    Transposition easy cipher (table) to UI.

--- app/src/main/java/com/makentoshe/chiperchan/model/cipher/TransposEasyTableCipher.kt
+++ app/src/main/java/com/makentoshe/chiperchan/model/cipher/TransposEasyTableCipher.kt
@@ -16,3 +16,29 @@ class TransposEasyTableCipher(private val key: IntArray) : Cipher {
 
     override fun decode(string: String) = encode(string)
 }
+class Factory : Cipher.Factory {
+
+    override val title = "Transposition easy cipher (table)"
+
+    override fun build(parameters: Map<String, Any>): TransposEasyTableCipher {
+        val key = (parameters["key"] as? String?)
+            ?: throw IllegalAccessException("`key` parameter is required and should be String")
+        var keyAr: Array <Int> = Array(key.length,{0})
+        var i = 0
+        for (c in key) {
+            keyAr[i] = c.toInt()
+            i++
+        }
+        return TransposEasyTableCipher(keyAr.toIntArray())
+    }
+
+    override fun getParameters(): List<Cipher.Parameter> {
+        return listOf(
+            Cipher.Parameter(
+                name = "key",
+                displayName = "Key",
+                spec = Cipher.Spec(Cipher.Type.String)
+            )
+        )
+    }
+}
