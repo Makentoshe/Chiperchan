9659ea97adae287d783618766da0e8bbdbc74952
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2020-05-13 16:49:23 +0300

    Add Bovykin cipher to UI

--- app/src/main/java/com/makentoshe/chiperchan/model/cipher/BovykinCipher.kt
+++ app/src/main/java/com/makentoshe/chiperchan/model/cipher/BovykinCipher.kt
@@ -28,4 +28,39 @@ class BovykinCipher(
         }
         return result
     }
+
+    class Factory : Cipher.Factory {
+
+        override val title = "Bovykin cipher"
+
+        override fun build(parameters: Map<String, Any>): BovykinCipher {
+            val key = (parameters["key1"] as? String?)
+                ?: throw IllegalAccessException("`key1` parameter is required and should be String")
+            val key2 = (parameters["key2"] as? String?)
+                ?: throw IllegalAccessException("`key2` parameter is required and should be String")
+            val key3 = (parameters["key3"] as? String?)
+                ?: throw IllegalAccessException("`key3` parameter is required and should be String")
+            return BovykinCipher(key, key2, key3)
+        }
+
+        override fun getParameters(): List<Cipher.Parameter> {
+            return listOf(
+                Cipher.Parameter(
+                    name = "key1",
+                    displayName = "Key 1",
+                    spec = Cipher.Spec(Cipher.Type.String)
+                ),
+                Cipher.Parameter(
+                    name = "key2",
+                    displayName = "Key 2",
+                    spec = Cipher.Spec(Cipher.Type.String)
+                ),
+                Cipher.Parameter(
+                    name = "key3",
+                    displayName = "Key 3",
+                    spec = Cipher.Spec(Cipher.Type.String)
+                )
+            )
+        }
+    }
 }
